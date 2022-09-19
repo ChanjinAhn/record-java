3c0a9be614e2c9c986fa051111e39fd689815e5c
Chajin Ahn <infilljean@gmail.com>
2022-09-19 14:06:37 +0900

    Feat: add odd even check
    - programmers odd even check class and test class

---
+++ app/src/test/java/programers/OddEvenTest.java
@@ -0,0 +1,41 @@
+package programers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import programers.OddEven.OddEven;
+
+public class OddEvenTest {
+
+
+    @DisplayName("홀수 Test")
+    @Test
+    void oddTest() {
+        // given
+        int num = 3;
+        OddEven oddEven = new OddEven();
+
+        // when
+        String answer = oddEven.solution(num);
+
+        // then
+        assertThat(answer).isEqualTo("Odd");
+    }
+
+    @DisplayName("짝수 Test")
+    @Test
+    void evenTest() {
+        //given
+        int num = 4;
+        OddEven oddEven = new OddEven();
+        // when
+        String answer = oddEven.solution(num);
+
+        // then
+        assertThat(answer).isEqualTo("Even");
+    }
+
+
+
+}
