c34cd84cb8346579e48a9e46754569e4b053b95c
chanjin-ahn <chanjin-ahn@nate.com>
2022-09-22 14:27:26 +0900

    Feat: add DigitArraySort
    - Selection Sort 메소드 추가
    - Selection Sort 테스트 추가

--- app/src/main/java/io/oikk/algorithm/sort/DigitArraySort.java
+++ app/src/main/java/io/oikk/algorithm/sort/DigitArraySort.java
@@ -1,30 +1,26 @@
 package io.oikk.algorithm.sort;
 
+import java.util.Arrays;
+
 public class DigitArraySort {
-
-
-    static int[] insertionSort(int[] numbers){
+    int[] insertionSort(int[] numbers){
         for (int i = 1; i < numbers.length; i++) {
-
             int number = numbers[i];
             int j = i -1 ;
-
             while (j >=0 && number < numbers[j]){
                 numbers[j+1] = numbers[j];
                 j--;
             }
-
             numbers[j+1] = number;
         }
-
         return numbers;
     }
 
-    static int[] bubbleSort(int[] numbers){
+    int[] bubbleSort(int[] numbers){
+        Arrays.sort(numbers);
         for (int i = numbers.length; i > 0; i--) {
             int j = i;
             int k = 0;
-
                 int z = numbers[k];
                 while (j > 0 && k+1 < i){
                     if (z < numbers[k+1]){
@@ -41,4 +37,18 @@ public class DigitArraySort {
         return numbers;
     }
 
+    int[] selectionSort(int[] numbers){
+        for (int i = 0; i < numbers.length-1; i++) {
+            int index = i;
+            for (int j = i+1; j < numbers.length; j++) {
+                if(numbers[j] < numbers[index]){
+                    index = j;
+                }
+            }
+            int temp = numbers[i];
+            numbers[i] = numbers[index];
+            numbers[index] = temp;
+        }
+        return numbers;
+    }
 }
--- app/src/test/java/io/oikk/algorithm/sort/DigitArraySortTest.java
+++ app/src/test/java/io/oikk/algorithm/sort/DigitArraySortTest.java
@@ -3,13 +3,35 @@ package io.oikk.algorithm.sort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 public class DigitArraySortTest {
 
-    static int[] numbers;
+    int[] numbers;
+    long beforeTime;
+    long afterTime;
+    long secDiffTime;
+
+    private final DigitArraySort sort = new DigitArraySort();
+
+
+    @Disabled
+    @BeforeEach
+    void beforeRunTimeCheck () {
+        beforeTime = System.currentTimeMillis();
+    }
+
+    @Disabled
+    @AfterEach
+    void afterRunTimeCheck() {
+        afterTime = System.currentTimeMillis();
+        secDiffTime = (afterTime - beforeTime);
+        System.out.println("메소드 실행 시간: " +secDiffTime);
+    }
 
     @DisplayName("삽입 정렬 시간 테스트")
     @Test
@@ -17,11 +39,12 @@ public class DigitArraySortTest {
         // given
         numbers = new int[]{5,2,4,1,7};
         // when
-        int[] result = DigitArraySort.insertionSort(numbers);
+        int[] result = sort.insertionSort(numbers);
 
         // then
         int[] thenResult = {1,2,4,5,7};
         assertThat(thenResult).isEqualTo(result);
+
     }
 
     @DisplayName("버블 정렬 시간 테스트")
@@ -29,15 +52,29 @@ public class DigitArraySortTest {
     void bubbleSortTest(){
         // given
         numbers = new int[]{8,3,7,5,4,2};
+
         // when
-        int[] result = DigitArraySort.bubbleSort(numbers);
+        int[] result = sort.bubbleSort(numbers);
 
         // then
         int[] thenResult = {2,3,4,5,7,8};
         assertThat(thenResult).isEqualTo(result);
     }
 
-//    @DisplayName("병합 테스트")
+    @DisplayName("선택 정렬 테스트")
+    @Test
+    void selectionSort () {
+        // given
+        numbers = new int[]{8,3,7,5,4,2};
+
+        // when
+        int[] result = sort.selectionSort(numbers);
+
+        // then
+        int[] thenResult = {2,3,4,5,7,8};
+        assertThat(thenResult).isEqualTo(result);
+    }
+//    @DisplayName("병합 테스트")2
 //    @Test
 //    void MergeTest(){
 //        //given
