0449280fadf4788926998dcd0fa146031930538c
chanjin-ahn <chanjin-ahn@nate.com>
2022-09-23 17:51:45 +0900

    Feat: add image util
    - 파일의 이름과 확장자 분리 기능 추가

---
+++ app/src/main/java/io/oikk/file/Image.java
@@ -0,0 +1,30 @@
+package io.oikk.file;
+
+import java.util.Map;
+import org.apache.commons.io.FilenameUtils;
+
+public class Image {
+    Map<String, String> substringFilenameExtension (String fileName) {
+        String filenameExtension = FilenameUtils.getExtension(fileName);
+        fileName = FilenameUtils.getBaseName(fileName);
+        return Map.of(
+            "fileName", fileName,
+            "extension", filenameExtension
+        );
+    }
+
+
+
+
+    void save (Image image, String hashedName){
+
+    }
+
+
+    Image find (String hashedName) {
+
+
+       return null;
+    }
+
+}
---
+++ app/src/test/java/io/oikk/file/FilenameExtensionSubStringTest.java
@@ -0,0 +1,42 @@
+package io.oikk.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FilenameExtensionSubStringTest {
+
+    @DisplayName("파일 한글이름 확장자 추출 테스트")
+    @Test
+    void koreanNameTest(){
+        // given
+        String fileName = "푸른 숲 하늘.jpg";
+        Image image = new Image();
+
+        // when
+        Map<String, String> filenameExtension = image.substringFilenameExtension(fileName);
+
+        // then
+        assertThat(filenameExtension.get("fileName")).isEqualTo("푸른 숲 하늘");
+        assertThat(filenameExtension.get("extension")).isEqualTo("jpg");
+    }
+
+    @DisplayName("파일 영어이름 확장자 추출 테스트")
+    @Test
+    void englishNameTest() {
+        // given
+        String fileName = "helloWorld.png";
+        Image image = new Image();
+
+        // when
+        Map<String, String> filenameExtension = image.substringFilenameExtension(fileName);
+
+        // then
+        assertThat(filenameExtension.get("fileName")).isEqualTo("helloWorld");
+        assertThat(filenameExtension.get("extension")).isEqualTo("png");
+    }
+
+
+}
