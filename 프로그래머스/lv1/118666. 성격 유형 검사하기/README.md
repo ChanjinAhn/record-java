ad21aac612effddd4f9689921c0f0bb9e209b128
olkk <97466353+ChanjinAhn@users.noreply.github.com>
2022-09-26 10:41:56 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 9.77 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=java) 
 
 ### 성능 요약
 
-메모리: 86.4 MB, 시간: 11.67 ms
+메모리: 79.8 MB, 시간: 9.77 ms
 
 ### 구분
 
--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,8 +1,10 @@
 class Solution {
     public String solution(String[] survey, int[] choices) {
        String[] types = {"RT", "CF", "JM", "AN"};
+        // A-Z 에 해당하는 int 배열 생성 (A:0, Z:26)
         int arr[] = new int[26];
         String answer = "";
+        // 검사지 순서와 정답을 비교 후 각 배열에 값을 더함
         for (int i = 0; i < survey.length; i++) {
             char a = survey[i].charAt(0);
             char b = survey[i].charAt(1);
@@ -13,6 +15,7 @@ class Solution {
                 arr[a - 'A'] += value;
             }
         }
+        // 짝 타입을 비교하여 더 높은 값의 코드를 출력
         for (String type : types) {
             char a = type.charAt(0);
             char b = type.charAt(1);
